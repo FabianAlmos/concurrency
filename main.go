1b805ee8be36a7c3204ead2975b90b6b49765a8d
Fábián Álmos <fabian.almos77@gmail.com>
2023-06-14 21:13:44 +0200

    Deleted old code

--- main.go
+++ main.go
@@ -1,23 +1,5 @@
 package main
 
-import (
-	worker "concuLec/workers"
-	"fmt"
-)
-
 func main() {
-	queue := worker.NewAsyncQueue()
 
-	l := []string{"1", "2", "3"}
-	queue.Start()
-
-	for i := 0; i < 2; i++ {
-		queue.Append(func() {
-			func(k string) {
-				fmt.Println("line", k)
-			}(l[i])
-		})
-	}
-
-	queue.Shutdown()
 }
